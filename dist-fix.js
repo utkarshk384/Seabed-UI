a88d43f17ff0c4db1a524a8ea5ecb17193dda11f
Utkarsh Kumar <utkarshk384@gmail.com>
2022-03-30 06:33:51 +0530

    Refactored code

--- dist-fix.js
+++ dist-fix.js
@@ -4,6 +4,7 @@ let content = fs.readFileSync("./dist/index.js").toString()
 
 fs.rmSync("./dist/index.js")
 
+// Replace exports because it doesn't seem to be supported
 content = content.replace(/exports.default/, "module.exports")
 fs.writeFileSync("./dist/index.js", content, "utf8")
 
